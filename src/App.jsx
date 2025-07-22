3eafe2ce9156e6f7710f7b22062442cb4dda8fcb
seifessam60 <seifessam60@gmail>
2025-07-22 19:34:05 +0300

    Menu Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
+import Art from "./components/Art";
+import Menu from "./components/Menu";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -14,6 +16,8 @@ const App = () => {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
+      <Menu />
     </main>
   );
 };
